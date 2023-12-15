36527ba85a6452df1cde555b14d922a1b90ca6b8
L0kayata <haoyx71@protonmail.com>
2023-12-15 01:43:15 +0100

    Script Modification

--- Alpha/server.sh
+++ Alpha/server.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-#SBATCH --time=00:05:00
+#SBATCH --time=00:15:00
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=1
 
-export SPARK_HOME=/home/dsys2313/yuanhao/kafka/kafka_2.13-3.6.0
-
-export PYSPARK_PYTHON=/var/scratch/dsys2313/miniconda3/bin/python
-
-$SPARK_HOME/bin/zookeeper-server-start.sh $SPARK_HOME/config/zookeeper.properties &
+zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
 
 echo 'zookeeper done'
 
-$SPARK_HOME/bin/kafka-server-start.sh $SPARK_HOME/config/server.properties &
+sleep 10
+
+kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 
 echo 'kafka done'
 
-sleep 30
+sleep 20
 
-$SPARK_HOME/bin/kafka-topics.sh --create --topic Topic1 --partitions 2 --replication-factor 1 --bootstrap-server localhost:9092
+kafka-topics.sh --create --topic Topic1 --partitions $3 --replication-factor 1 --bootstrap-server localhost:9092
 
 echo 'topic 1 done'
 
-$SPARK_HOME/bin/kafka-topics.sh --create --topic Topic2 --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
+kafka-topics.sh --create --topic Topic2 --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
 
 echo 'topic 2 done'
 
@@ -29,6 +26,8 @@ node1=`squeue | grep dsys2313 | awk -F ' ' '{print $8}'`
 
 echo $node1
 
-$PYSPARK_PYTHON distributor.py $node1
+sleep 5
 
-$PYSPARK_PYTHON integrator.py $node1
+python distributor.py $node1
+
+python integrator.py $node1
--- Alpha/worker.sh
+++ Alpha/worker.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=00:02:00
-
-# Load the necessary modules
-
-# Set the Spark home directory
-export SPARK_HOME=/home/dsys2313/yuanhao/spark_test/spark-3.5.0-bin-hadoop3
-
-# Set the Python executable for PySpark
-export PYSPARK_PYTHON=/var/scratch/dsys2313/miniconda3/bin/python
+#SBATCH --nodes=$3
+#SBATCH --time=00:15:00
 
 # Start the PySpark application using srun
-$SPARK_HOME/bin/spark-submit  processor.py $1
+
+spark-submit  processor.py $1 $4
