de35cb49dc28d92e8decfcfa7da4f3bff9f480be
L0kayata <haoyx71@protonmail.com>
2023-12-15 02:26:14 +0100

    bug fix

--- Alpha/worker.sh
+++ Alpha/worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes=$3
+#SBATCH --nodes=2
 #SBATCH --time=00:15:00
 
 # Start the PySpark application using srun
 
-spark-submit  processor.py $1 $4
+spark-submit processor.py $1 $4
